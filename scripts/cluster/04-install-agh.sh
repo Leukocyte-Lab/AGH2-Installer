06986c08d6e0d097877ae3e4aaae4bf1d29b1660
aries0d0f <aries0d0f@gmail.com>
2022-03-23 09:37:03 +0800

    feat: Add timeout to waiting for cluster initialize.

--- scripts/cluster/04-install-agh.sh
+++ scripts/cluster/04-install-agh.sh
@@ -5,3 +5,6 @@ cat /tmp/deployment.yaml
 helm repo add lkclab https://charts.lkc-lab.com/ && \
 helm repo update && \
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml helm install agh lkclab/agh2-helm -f /tmp/deployment.yaml --namespace agh --create-namespace
+
+echo "Waiting for agh to be ready"
+sleep 120
