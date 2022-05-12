ecb9cd70a48500f3f16b63a59e733b59824105fc
Bevis <abc123123437@gmail.com>
2022-05-12 07:03:58 +0000

    chore: add seed file

---
+++ artifacts/db/ATTACK-mitre_groups.sql
@@ -0,0 +1,31 @@
+INSERT INTO public.mitre_groups (id,created_at,updated_at,deleted_at,techniques,exploit_id,name,description,steps,targets) VALUES
+	 ('0efb0d84-59dd-4a07-9b76-973f67f9eab2','2022-01-06 13:50:41.283591+08','2022-01-06 13:50:41.283591+08',NULL,'{"T1213.002","T1059.004"}','dc1f6262-cd58-4659-9f7e-3a88fe4ad6a8','MitreGroup-2','Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit','@->@: Generate wordlist to start brute force @->target-3: Start brute force login @->target-3: Remote login with `HTTP` protocol target-3->target-3: Disable firewall @->target-3: Download files in `FTP` via `anonymous` account and try to find password @->target-4: Start `SMB` brute force login @->target-4: Remote login with `HTTP` protocol target-4->target-4: Modify registry to disable defender @->target-4: Upload `mimikatz` and run it to find domain admin’s `NTLM` hash @->@: Re-generate 20 wordlist to start brute force ','[{"ip": "", "os": "Linux", "name": "target-3"}, {"ip": "", "os": "WIN10", "name": "target-4"}]'),
+	 ('1d71b41d-0e4b-450c-91cf-8893373701df','2022-01-06 13:50:41.27764+08','2022-01-06 13:50:41.27764+08',NULL,'{"T1548.001","T1499.001"}','d7bc05e7-c6a5-403a-b966-b85982b378ac','MitreGroup-1','Lorem Ipsum is simply dummy text of the printing and typesetting industry.','@->AGH-Group-Lisa: Port Scan
+AGH-Group-John->AGH-Group-Lisa: Connect the Waterhole
+AGH-Group-John->@: Spawn a reverse shell with encryption protocols','[{"ip": "", "os": "ArgusHack", "name": "@"}, {"os": "WINDOWS_9_64", "name": "AGH-Group-Lisa"}, {"os": "WINDOWS_7_64", "name": "AGH-Group-John"}]'),
+	 ('2189560d-6f6f-4c33-88a8-2b6d802cf06a','2022-01-06 13:50:41.27764+08',NULL,NULL,'{"T1071.001","T1217","T1110","T1115","T1059.001","T1059.003","T1059.005","T1543.003","T1485","T1486"}','f6e31497-2a13-4809-9856-9d0f5e86feef','APT38','[APT38](https://attack.mitre.org/groups/G0082) is a North Korean state-sponsored threat group that specializes in financial cyber operations; it has been attributed to the Reconnaissance General Bureau.[1] Active since at least 2014, APT38 has targeted banks, financial institutions, casinos, cryptocurrency exchanges, SWIFT system endpoints, and ATMs in at least 38 countries worldwide. Significant operations include the 2016 Bank of Bangladesh heist, during which APT38 stole $81 million, as well as attacks against Bancomext (2018) and Banco de Chile (2018); some of their attacks have been destructive.','@->Web Server: Port Scan
+@->Web Server: Exploit with `CVE-2017-5638`
+@->Web Server: Upload `jspspy` and run it
+@->Web Server: Start Web Service as Waterhole (`CVE-2016-0189`)
+John PC->Web Server: Connect the Waterhole
+John PC->@: Spawn a reverse shell with encryption protocols
+@->John PC: Exec `net user /domain` to get domain and user list
+@->John PC: Find password in clipboard
+@->@: Generate wordlist to start brute force
+@->Peter PC: Start brute force login
+@->Peter PC: Remote login with `HTTP` protocol
+Peter PC->Peter PC: Disable firewall
+@->Peter PC: Download files in `FTP` via `anonymous` account and try to find password
+@->Lisa PC: Start `SMB` brute force login
+@->Lisa PC: Remote login with `HTTP` protocol
+Lisa PC->Lisa PC: Modify registry to disable defender
+@->Lisa PC: Upload `mimikatz` and run it to find domain admin’s `NTLM` hash
+@->@: Re-generate 20 wordlist to start brute force
+@->Leon PC: Start `SMB` brute force login
+@->Leon PC: Remote login with `HTTP` protocol
+Leon PC->Leon PC: Set `%TEMP%` as defender''s exclude path
+@->Leon PC: Upload `mimikatz` and run it to find domain admin’s `NTLM` hash (success)
+@->AD Server: Remote login with encryption protocols
+@->AD Server: Upload ransomware and run it to encryption files
+@->AD Server: Clear Windows event log
+','[{"ip": "", "os": "ArgusHack", "name": "@", "status": "STATUS_PENDING"}, {"ip": "", "os": "Ubuntu Server 16.04", "name": "Web Server", "status": "STATUS_PENDING"}, {"ip": "", "os": "Windows 7", "name": "John PC", "status": "STATUS_PENDING"}, {"ip": "", "os": "Windows 10", "name": "Peter PC", "status": "STATUS_PENDING"}, {"ip": "", "os": "Windows 10", "name": "Lisa PC", "status": "STATUS_PENDING"}, {"ip": "", "os": "Windows 10", "name": "Leon PC", "status": "STATUS_PENDING"}, {"ip": "", "os": "Windows Server 2012", "name": "AD Server", "status": "STATUS_PENDING"}]');
---
+++ artifacts/db/ATTACK-mitre_techniques.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.mitre_techniques (id,created_at,updated_at,deleted_at,description,technique_id,os_name,exploit_id) VALUES
+	 ('8da8f980-4100-4e5b-b7c4-aaacc5c4d627','2022-01-06 13:50:41.271598+08','2022-01-06 13:50:41.271598+08',NULL,NULL,'T1548.001','windows','2905aa06-1dd2-11b2-8000-080027b246c3'),
+	 ('52e4330a-d796-486f-a87e-4de3d977c85e','2022-01-06 13:50:41.276077+08','2022-01-06 13:50:41.276077+08',NULL,NULL,'T1499.001','linux','b94418aa-10c3-43e6-be5c-c83ed6ec6371'),
+	 ('f8dbf261-a4ce-4009-97c1-a6e549f05994','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1071.001','linux','88a4db62-cf9f-482d-9789-4d064707fab9'),
+	 ('bc91741b-be8a-4622-8aea-c8db7fd2a1fd','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1217','windows','a5626cda-4df4-4bf9-b411-0edbf5c913e1'),
+	 ('55878c1b-5422-42ad-9ff9-4e5e9b37db19','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1110','windows','95209e93-67ec-40e7-99b7-6a780f172743'),
+	 ('c41434b6-d10c-4217-972c-e4b16787285b','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1115','windows','081ca870-4234-4f60-8953-ac1f05d517d8'),
+	 ('ff5b1818-8c17-480a-9f14-f4e5d2773437','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1059.001','windows','7f770a72-347d-49c2-b2b9-44c9e42fc1c8'),
+	 ('1fae4cba-0923-40e6-80f8-95a928dfb0ac','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1059.003','windows','4439bcba-6d38-4621-ac12-9f845d53c5d5'),
+	 ('25893154-11a3-4f0a-a9fa-e13448dac0c3','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1059.005','windows','c34e5bcd-48a8-40b2-9856-23a477d6c06f'),
+	 ('89c2bad5-e5dd-4924-a42c-63016aa1be07','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1543.003','windows','b0fc7f67-6e81-4af9-aaa3-151120830554');
+INSERT INTO public.mitre_techniques (id,created_at,updated_at,deleted_at,description,technique_id,os_name,exploit_id) VALUES
+	 ('ca2a806a-b645-460b-9641-359bbfc51af7','2022-01-06 13:50:41.276077+08',NULL,NULL,NULL,'T1485','linux','59d57593-43e4-4742-9484-28486eae56ce');
