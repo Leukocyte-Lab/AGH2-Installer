f823d87b3d1ec4102c8203803783241afc53615a
Bevis <abc123123437@gmail.com>
2022-05-13 03:04:42 +0000

    feat: Add wait time for making installation ready.

--- scripts/db/00-install-postgress.sh
+++ scripts/db/00-install-postgress.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-add-apt-repository -y universe
+add-apt-repository -y universe && \
 add-apt-repository -y ppa:groonga/ppa
 
+sleep 10
+
 apt install -y wget lsb-release
+
+sleep 10
+
 wget https://packages.groonga.org/ubuntu/groonga-apt-source-latest-$(lsb_release --codename --short).deb
+
+sleep 30
+
 apt install -y -V ./groonga-apt-source-latest-$(lsb_release --codename --short).deb
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release --codename --short)-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+
+sleep 10
+
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release --codename --short)-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list && \
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+sleep 10
 
 apt update
 apt install -y -V postgresql-13-pgdg-pgroonga
+
+sleep 60
