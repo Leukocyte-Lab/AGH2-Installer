ccc30c3ce53d02890f387068968cdad210759cd3
Bevis <abc123123437@gmail.com>
2022-05-17 06:43:10 +0000

    feat: install agh db

---
+++ scripts/manul/env.sh
@@ -0,0 +1,12 @@
+export AGH_DB_IP=10.13.2.24
+export AGH_DB_USER=db-user
+export AGH_DB_PASSWORD=db-password
+
+export MINIO_ROOT_USER=minio
+export MINIO_ROOT_PASSWORD=minio-password    # 此處請使用包含英文字母大小寫及數字、特殊符號且長度大於10之密碼
+export DB_USER=agh
+export DB_PASSWORD=password
+export MINIO_CORE_USER=core-minio-user
+export MINIO_CORE_PASSWORD=core-minio-password
+export MINIO_CAPT_USER=capt-minio-user
+export MINIO_CAPT_PASSWORD=capt-minio-password
---
+++ scripts/manul/manul.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt install sshpass
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+filename=$SCRIPTPATH/env.sh
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    eval $line
+done < $filename
+
+sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_groups.sql $AGH_DB_USER@$AGH_DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_techniques.sql $AGH_DB_USER@$AGH_DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-os.sql $AGH_DB_USER@$AGH_DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/attack.sql $AGH_DB_USER@$AGH_DB_IP:/tmp/
+
+sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/json/IAMpolicy.json $AGH_DB_USER@$AGH_DB_IP:/tmp/
+
+sshpass -p "$AGH_DB_PASSWORD" scp -r $SCRIPTPATH/../../scripts/ $AGH_DB_USER@$AGH_DB_IP:/tmp/script/
+
+sshpass -p "$AGH_DB_PASSWORD" ssh -t $AGH_DB_USER@$AGH_DB_IP "echo $AGH_DB_PASSWORD | sudo -S chmod -R +x /tmp/script/;"
+
+sshpass -p "$AGH_DB_PASSWORD" ssh -t $AGH_DB_USER@$AGH_DB_IP "
+echo $AGH_DB_PASSWORD | sudo -S /./tmp/script/manul/db-entry.sh"
