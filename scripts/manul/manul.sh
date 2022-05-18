90b920f524f45ef4201ca9afacf0cb615c1b1c19
Bevis <abc123123437@gmail.com>
2022-05-18 03:16:49 +0000

    feat: auto gen known_hosts when first times

--- scripts/manul/manul.sh
+++ scripts/manul/manul.sh
@@ -9,7 +9,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     eval $line
 done < $filename
 
-sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_groups.sql $AGH_DB_USER@$DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp -o StrictHostKeyChecking=no $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_groups.sql $AGH_DB_USER@$DB_IP:/tmp/
 sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_techniques.sql $AGH_DB_USER@$DB_IP:/tmp/
 sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-os.sql $AGH_DB_USER@$DB_IP:/tmp/
 sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/attack.sql $AGH_DB_USER@$DB_IP:/tmp/
@@ -23,7 +23,7 @@ sshpass -p "$AGH_DB_PASSWORD" ssh -t $AGH_DB_USER@$DB_IP "echo $AGH_DB_PASSWORD 
 
 
 
-sshpass -p "$AGH_MASTER_PASSWORD" scp -r $SCRIPTPATH/../../scripts/ $AGH_MASTER_USER@$IP:/tmp/script/
+sshpass -p "$AGH_MASTER_PASSWORD" scp -o StrictHostKeyChecking=no -r $SCRIPTPATH/../../scripts/ $AGH_MASTER_USER@$IP:/tmp/script/
 
 sshpass -p "$AGH_MASTER_PASSWORD" ssh -t $AGH_MASTER_USER@$IP "echo $AGH_MASTER_PASSWORD | sudo -S chmod -R +x /tmp/script/;"
 
