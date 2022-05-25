ce00affe386cffc6a9aafd8f2593b19cd0a79312
aries0d0f <aries0d0f@gmail.com>
2022-05-25 15:33:32 +0800

    feat: Check more carefully on cleanup provision stage.

--- scripts/cleanup/00-cleanup.sh
+++ scripts/cleanup/00-cleanup.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
-vim-cmd vmsvc/getallvms | grep "AGH-" | awk '{ print "vim-cmd vmsvc/power.off " $1 }' | sh && \
-vim-cmd vmsvc/getallvms | grep "AGH-" | awk '{ print "vim-cmd vmsvc/unregister " $1 }' | sh
-
-sleep 10
-
+echo "Current VM(s) on ESXi Host" && \
 vim-cmd vmsvc/getallvms
 
-sleep 10
+sleep 1
 
-cp "/vmfs/volumes/$DATASTORE/packer_cache/*.iso" /tmp && \
-rm -rf "/vmfs/volumes/$DATASTORE/AGH-*" && \
-rm -rf "/vmfs/volumes/$DATASTORE/packer_cache/" && \
-mkdir -p "/vmfs/volumes/$DATASTORE/packer_cache/" && \
-cp "/tmp/*.iso /vmfs/volumes/$DATASTORE/packer_cache"
+echo "Power Off existed VM..." && \
+vim-cmd vmsvc/getallvms | grep "AGH-" | awk '{ print "vim-cmd vmsvc/power.off " $1 }' | sh && \
+echo "Unregister existed VM..." && \
+vim-cmd vmsvc/getallvms | grep "AGH-" | awk '{ print "vim-cmd vmsvc/unregister " $1 }' | sh
+
+sleep 1
+
+echo "Current VM(s) on ESXi Host" && \
+vim-cmd vmsvc/getallvms
+
+sleep 1
+
+echo "Delete existed VM file in datastore..." && \
+rm -rf "/vmfs/volumes/$DATASTORE/AGH-*"
+
+sleep 1
+
+if [ -d "/vmfs/volumes/$DATASTORE/packer_cache/" ]; then
+  echo "Backup ISO image..."
+  cp -a "/vmfs/volumes/$DATASTORE/packer_cache/*.iso" /tmp 2>/dev/null || :
+  echo "Cleanup Packer cache..."
+  rm -rf "/vmfs/volumes/$DATASTORE/packer_cache/"
+  echo "Restore ISO image..."
+  mkdir -p "/vmfs/volumes/$DATASTORE/packer_cache/"
+  cp -a "/tmp/*.iso" "/vmfs/volumes/$DATASTORE/packer_cache" 2>/dev/null || :
+fi
