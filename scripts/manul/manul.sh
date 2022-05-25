c7ac86c278bfc23ce0371938c884cc0bc6bb1277
Bevis <abc123123437@gmail.com>
2022-05-25 08:35:51 +0000

    feat: $SCRIPTPATH double quote

--- scripts/manul/manul.sh
+++ scripts/manul/manul.sh
@@ -4,26 +4,26 @@ sudo apt install sshpass
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-filename=$SCRIPTPATH/env.sh
+filename="$SCRIPTPATH"/env.sh
 while IFS='' read -r line || [[ -n "$line" ]]; do
     eval $line
 done < $filename
 
-sshpass -p "$AGH_DB_PASSWORD" scp -o StrictHostKeyChecking=no $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_groups.sql $AGH_DB_USER@$DB_IP:/tmp/
-sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-mitre_techniques.sql $AGH_DB_USER@$DB_IP:/tmp/
-sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/ATTACK-os.sql $AGH_DB_USER@$DB_IP:/tmp/
-sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/db/attack.sql $AGH_DB_USER@$DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp -o StrictHostKeyChecking=no "$SCRIPTPATH"/../../artifacts/db/ATTACK-mitre_groups.sql $AGH_DB_USER@$DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp "$SCRIPTPATH"/../../artifacts/db/ATTACK-mitre_techniques.sql $AGH_DB_USER@$DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp "$SCRIPTPATH"/../../artifacts/db/ATTACK-os.sql $AGH_DB_USER@$DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp "$SCRIPTPATH"/../../artifacts/db/attack.sql $AGH_DB_USER@$DB_IP:/tmp/
 
-sshpass -p "$AGH_DB_PASSWORD" scp $SCRIPTPATH/../../artifacts/json/IAMpolicy.json $AGH_DB_USER@$DB_IP:/tmp/
+sshpass -p "$AGH_DB_PASSWORD" scp "$SCRIPTPATH"/../../artifacts/json/IAMpolicy.json $AGH_DB_USER@$DB_IP:/tmp/
 
-sshpass -p "$AGH_DB_PASSWORD" scp -r $SCRIPTPATH/../../scripts/ $AGH_DB_USER@$DB_IP:/tmp/script/
+sshpass -p "$AGH_DB_PASSWORD" scp -r "$SCRIPTPATH"/../../scripts/ $AGH_DB_USER@$DB_IP:/tmp/script/
 
 sshpass -p "$AGH_DB_PASSWORD" ssh -t $AGH_DB_USER@$DB_IP "echo $AGH_DB_PASSWORD | sudo -S chmod -R +x /tmp/script/;"
 
 
 
 
-sshpass -p "$AGH_MASTER_PASSWORD" scp -o StrictHostKeyChecking=no -r $SCRIPTPATH/../../scripts/ $AGH_MASTER_USER@$IP:/tmp/script/
+sshpass -p "$AGH_MASTER_PASSWORD" scp -o StrictHostKeyChecking=no -r "$SCRIPTPATH"/../../scripts/ $AGH_MASTER_USER@$IP:/tmp/script/
 
 sshpass -p "$AGH_MASTER_PASSWORD" ssh -t $AGH_MASTER_USER@$IP "echo $AGH_MASTER_PASSWORD | sudo -S chmod -R +x /tmp/script/;"
 
