dbbc5a2d5ce7fc872d2c25ca9f8a42c73d98b1f6
aries0d0f <aries0d0f@gmail.com>
2022-05-26 11:54:37 +0800

    fix: PostgreSQL install stage.

--- scripts/db/00-install-postgress.sh
+++ scripts/db/00-install-postgress.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-cp /tmp/sources.list /etc/apt/sources.list
-apt update
-
 add-apt-repository -y universe && \
-add-apt-repository -y ppa:groonga/ppa
+add-apt-repository -y ppa:groonga/ppa && \
+apt update && \
+apt install -y wget lsb_release
 
 sleep 10
 
-apt install -y wget lsb-release
+wget https://packages.groonga.org/ubuntu/groonga-apt-source-latest-$(lsb_release -cs).deb
 
 sleep 10
 
-wget https://packages.groonga.org/ubuntu/groonga-apt-source-latest-$(lsb_release --codename --short).deb
-
-sleep 30
-
-apt install -y -V ./groonga-apt-source-latest-$(lsb_release --codename --short).deb
+apt install -y ./groonga-apt-source-latest-$(lsb_release -cs).deb
 
 sleep 10
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release --codename --short)-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list && \
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list && \
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 sleep 10
 
-apt update
-apt install -y -V postgresql-13-pgdg-pgroonga
+apt update && \
+apt install -y postgresql-13-pgdg-pgroonga
 
 sleep 60
