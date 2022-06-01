54a99c19760379ff4ed3a1adfbddb790a6c1766a
aries0d0f <aries0d0f@gmail.com>
2022-06-01 17:55:16 +0800

    chores: Change out directory path.

--- scripts/license/00-get-license.sh
+++ scripts/license/00-get-license.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p ./out
+
 echo -ne "\033[34mPlease enter Account ID: \033[0m"
 read account_id
 echo -ne "\033[34mPlease enter License ID: \033[0m"
@@ -9,4 +11,4 @@ read token
 
 curl -X GET https://api.keygen.sh/v1/accounts/`echo $account_id`/licenses/`echo $license_id`/actions/check-out \
   -H 'Accept: application/vnd.api+json' \
-  -H "Authorization: Bearer `echo $token`" > out/license.lic
+  -H "Authorization: Bearer `echo $token`" > ./out/license.lic
