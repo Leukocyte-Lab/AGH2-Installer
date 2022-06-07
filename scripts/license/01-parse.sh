8c101e296fe4eb3d138db70550a3b48370d0d29a
aries0d0f <aries0d0f@gmail.com>
2022-06-07 17:06:50 +0800

    fix: License parser.

--- scripts/license/01-parse.sh
+++ scripts/license/01-parse.sh
@@ -6,4 +6,4 @@ cat ./out/license.lic | sed -n '/BEGIN/,/END/ { //!p; }' | tr '\n' ' ' | sed 's/
 echo "key/license/`cat ./out/license.json | jq -r .enc`.`cat ./out/license.json | jq -r .sig`" > ./out/license.key && \
 cat ./out/license.json | jq -r .sig | base64 --decode > ./out/signature.bin && \
 echo -ne "license/" > ./out/license.bin && \
-cat ./out/license.json | jq -r .enc | tr '_-\n' '/+ ' | sed 's/ //g' >> ./out/license.bin
+cat ./out/license.json | jq -r .enc | tr '_-' '/+ ' | tr '\n' ' ' | sed 's/ //g' >> ./out/license.bin
