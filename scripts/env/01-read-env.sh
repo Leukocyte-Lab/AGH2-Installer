a5bd20f87eb6e4b38146e2dd21a7ce28e34bb2eb
aries0d0f <aries0d0f@gmail.com>
2022-06-07 16:53:44 +0800

    refactor: Abstractlize env operators.

--- scripts/env/01-read-env.sh
+++ scripts/env/01-read-env.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-secrets=`cat out/secret.json | jq -c '. | to_entries'`;
+read_env() {
+  secrets=`cat ./out/$1.json | jq -c '. | to_entries'`;
 
-for secret in $(echo "${secrets}" | jq -r '.[] | @base64'); do
-  _jq() {
-    echo ${secret} | base64 --decode | jq -r ${1}
-  }
+  for secret in $(echo "${secrets}" | jq -r '.[] | @base64'); do
+    _jq() {
+      echo ${secret} | base64 --decode | jq -r ${1}
+    }
 
-  printenv | grep "$(_jq .key)="
-done
+    printenv | grep "$(_jq .key)="
+  done
+}
