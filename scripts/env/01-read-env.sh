a72ea8ceb5de94b0b433d0c64756fc30e786b2a1
aries0d0f <aries0d0f@gmail.com>
2022-06-07 17:40:53 +0800

    fix: `read_env` no need to be a function.

--- scripts/env/01-read-env.sh
+++ scripts/env/01-read-env.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-read_env() {
-  secrets=`cat ./out/$1.json | jq -c '. | to_entries'`;
+secrets=`cat ./out/$1.json | jq -c '. | to_entries'`;
 
-  for secret in $(echo "${secrets}" | jq -r '.[] | @base64'); do
-    _jq() {
-      echo ${secret} | base64 --decode | jq -r ${1}
-    }
+for secret in $(echo "${secrets}" | jq -r '.[] | @base64'); do
+  _jq() {
+    echo ${secret} | base64 --decode | jq -r ${1}
+  }
 
-    printenv | grep "$(_jq .key)="
-  done
-}
+  printenv | grep "$(_jq .key)="
+done
