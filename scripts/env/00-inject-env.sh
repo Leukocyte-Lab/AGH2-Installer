488f30721d94252071a4eb01bc4ee867e2ebadef
aries0d0f <aries0d0f@gmail.com>
2022-06-07 17:40:16 +0800

    fix: `inject_env` no need to be a function.

--- scripts/env/00-inject-env.sh
+++ scripts/env/00-inject-env.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-inject_env() {
-  secrets=`cat ./out/$1.json | jq -c '. | to_entries'`;
+secrets=`cat ./out/$1.json | jq -c '. | to_entries'`;
 
-  for secret in $(echo "${secrets}" | jq -r '.[] | @base64'); do
-    _jq() {
-      echo ${secret} | base64 --decode | jq -r ${1}
-    }
+for secret in $(echo "${secrets}" | jq -r '.[] | @base64'); do
+  _jq() {
+    echo ${secret} | base64 --decode | jq -r ${1}
+  }
 
-    export "SECRET_$(_jq .key)=$(_jq .value)"
-  done
-}
+  export "$2$(_jq .key)=$(_jq .value)"
+done
